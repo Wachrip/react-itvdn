2f44c6567f7db0a6418da506dab07b565bfd8e57
Dmitry <svardmitriy@gmail.com>
2022-01-01 16:43:41 +0600

    2nd task added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Kobe from "./components/kobe";
+import ShowString from "./components/showStrings";
 
 function App() {
   return (
     <div className="App">
       <Kobe text="Black mamba is always right here" />
+      <ShowString ginni="Gianna Bryant" kobe="Kobe Bryant" />
     </div>
   );
 }
---
+++ src/components/showStrings.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+let ShowString = (props) => {
+  return (
+    <div>
+      <div>{props.kobe}</div>
+      <div>{props.ginni}</div>
+    </div>
+  );
+};
+
+export default ShowString;
