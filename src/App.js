a7135a29c1f224290ad06c0700bc770c64951103
Dmitry <svardmitriy@gmail.com>
2022-01-03 00:50:26 +0600

    10th task added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import React from "react";
+import { ThemeContext, themes } from "./components/context/themeContext";
+import ThemedButton from "./components/context/themedButton";
 
-import Kobe from "./components/kobe";
-import Main from "./components/todo";
+function Toolbar(props) {
+  return <ThemedButton onClick={props.changeTheme}>Change Theme</ThemedButton>;
+}
 
-function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/kobe" element={<Kobe />} />
-      </Routes>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: themes.light,
+    };
+
+    this.toggleTheme = () => {
+      this.setState((state) => ({
+        theme: state.theme === themes.dark ? themes.light : themes.dark,
+      }));
+    };
+  }
+
+  render() {
+    return (
+      <div style={{ backgroundColor: this.state.theme.background }}>
+        <ThemeContext.Provider value={this.state.theme}>
+          <Toolbar changeTheme={this.toggleTheme} />
+        </ThemeContext.Provider>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+const ThemeContext = React.createContext("light");
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
